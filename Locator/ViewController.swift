8b3efd618c4dbff0f0d4a96dc17a82a461554769
Peter van de Koolwijk <kooltjes@yahoo.com>
2017-12-27 17:02:18 +0100

    Initial Commit

---
+++ Locator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locator
+//
+//  Created by Pkoolwijk on 27/12/2017.
+//  Copyright © 2017 Blue Dolfin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
